95cbf22f277371c409fcce3d0b9a12676bc03092
Melliti <r.melliti@protonmail.com>
2019-12-28 00:40:50 -0500

    [Add] textfield only accept digits + addition completed

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,31 +1,35 @@
 package sample;
 
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Controller {
     public TextField t1;
     public Label l1;
     public Text result;
+    public Text value = new Text("");
     public Button button;
-    public List<Integer> input = new ArrayList<Integer>();
+
+    @FXML public void formatter() {
+        if (!Character.isDigit(t1.getText().charAt(t1.getText().length() - 1)))
+            t1.setText(value.getText());
+        else
+            value.setText(t1.getText());
+    }
 
     public void add() {
-        if (input.size() == 0)
-            input.add(Integer.parseInt(t1.getText()));
+        System.out.println(result);
+        if (result.getText() == "")
+            result.setText(t1.getText());
+        else
+            result.setText(String.valueOf(Integer.parseInt(result.getText()) + Integer.parseInt(t1.getText())));
+        t1.clear();
     }
 
     public void minus() {
-        if (input.size() == 0)
-            input.add(Integer.parseInt(t1.getText()));
-        else {
-            result.setText(String.valueOf(input.get(0) + Integer.parseInt(t1.getText())));
-            input.clear();
-        }
+
     }
 }
